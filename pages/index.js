0c26438b48d0bf7d39bd779aa0d52247e80d834f
Erasto73 <erastochina@gmail.com>
2023-09-20 15:41:21 +0300

    second commit

--- pages/index.js
+++ pages/index.js
@@ -50,8 +50,8 @@ export default function Home() {
         </button>
 
       </div>
-       <div className= "mx-auto bg-gradient-to-b from-teal-500 rounded-full w-8 h-80 relative overflow-hidden mt-10 md:h-96 md:w-96">
-         <Image src = "/erasto.png" alt='photo' layout = "fill" objectFit = "cover"/>
+       <div className= "mx-auto bg-gradient-to-b from-teal-500 rounded-full w-8 h-80 relative overflow-hidden mt-10 md:h-96 md:w-96 ">
+         <Image src = "/erasto.png" alt='photo' fill  objectFit = "cover" />
       </div>
     
     
@@ -65,21 +65,21 @@ export default function Home() {
       </div>
       <div className = "lg:flex gap-10">
         <div className = "text-center shadow-lg p-10 rounded-xl my-10 flex-1">
-          <span><Image src = "/design.png" alt="design" width={100} height={100} /></span>
+          <Image src = "/design.png" alt="design" width={100} height={100} />
           <h3 className= "text-lg font-medium pt-8 pb-2">Beautiful Design</h3>
           <p className= "py-2">
             Creating elegant designs suited for your needs following core design theory.
           </p>
         </div>
         <div className= "text-center p-10 shadow-lg rounded-xl my-10 flex-1">
-         <span><Image src = "/code.png" alt="code" width={100} height={100} /></span>
+         <Image src = "/code.png" alt="code" width={100} height={100} />
          <h3 className= "text-lg font-medium pt-8 pb-2">Code your dream project</h3>
          <p className= "py-2">
           Do you have an idea for your next website lets make it a reality.
          </p>
         </div>
         <div className= "text-center shadow-lg p-10 rounded-xl my-10 flex-1">
-         <span><Image src = "/consulting.png" alt="consulting" width={100} height={100} /></span>
+         <Image src = "/consulting.png" alt="consulting" width={100} height={100} />
          <h3 className="text-lg font-medium pt-8 pb-2">Consulting</h3>
          <p className="py-2">
           Are you interested in feedback for your curent project I can give tips and tricks
@@ -125,29 +125,29 @@ export default function Home() {
     <div className = " gap-10 py-10 flex flex-row rounded-xl">
      <div className = "basis-1/3 flex-1">
       <a target = "_blank" rel = "noreferrer" href = "https://signaxcom-birthday-project.netlify.app/">
-      <span><Image className = " rounded-g object-cover"
+      <Image className = " rounded-g object-cover"
        width = {100}
        height = {100}
        src = "/pic1.png" alt = "pic1"
-       layout = "responsive" /></span>
+       layout = "responsive" />
       </a>
      </div>
      <div className = "basis-1/3 flex-1">
      <a target = "_blank" rel = "noreferrer" href = "https://next-events-teal.vercel.app/">
-      <span><Image className = "  rounded-g object-cover"
+      <Image className = "  rounded-g object-cover"
        width = {100}
        height = {100}
        src = "/pic2.png" alt = "pic2"
-       layout = "responsive" /></span>
+       layout = "responsive" />
      </a>
      </div>
      <div className =  "basis-1/3 flex-1">
      <a target = "_blank" rel = "noreferrer" href = "https://next-quiz-app-zeta.vercel.app/">
-      <span><Image className = " rounded-g object-cover"
+      <Image className = " rounded-g object-cover"
        width = {100}
        height = {100} 
        src = "/pic3.png" alt = "pic3" 
-       layout = "responsive"/></span>
+       layout = "responsive" />
      </a>
      </div>
     </div>
